f3496ee013b6471884d55c69cadf9f938d12b7a9
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-18 00:26:08 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,4 +3,15 @@ def sort_array_asc(array)
 end
 sort_array_asc([4,1,6,3,7])
 
-def sort_array_desc
+def sort_array_desc(array)
+array.each sort |a,b|
+if 
+  a==b
+  0
+  elsif
+  a>b
+  0
+else
+  a<b
+  1
+end
