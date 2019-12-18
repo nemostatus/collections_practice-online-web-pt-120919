2f5ad413eb7afcac4fcf5b75b209fc9b6e3ed445
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-18 00:45:29 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,15 +3,15 @@ def sort_array_asc(array)
 end
 sort_array_asc([4,1,6,3,7])
 
-def sort_array_desc(array)
-array.each sort |a,b|
+def sort_array_desc(array = [1,2,3,4,5])
+array.sort do |a,b|
 if a==b
   0
   elsif a<b
   1
-elsif a>b
+  elsif a>b
   -1
 end
 end
 end
-sort_array_desc([1,2,3,4,5])
+sort_array_desc
